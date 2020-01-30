ef9e86a625b0914f548d0a6213d1e4584e073c4f
ainatancinco <56050499+ainatancinco@users.noreply.github.com>
2020-01-29 20:10:29 -0800

    Add files via upload
    Gary's stuff

---
+++ Blueprint/src/bpData.js
@@ -0,0 +1,24 @@
+import pic1 from "./picSrc/facebook.png"; {/* import pic# */}
+
+const blueprintData = [
+    {
+        id: 1,
+        title: "15 Best Blogs To Follow About Web Design",
+        img: pic1,
+        context: "Tenetur quidem quidem autem quaerat tempora cupiditate. Et consequatur eaque. Quidem et labore adipisci dolorum et eum expedita praesentium. Aliquid atque itaque accusantium maiores. Quasi architecto eum sequi dolores exercitationem incidunt molestiae. Quidem dolorem autem laborum vel voluptatem ut.",
+    },
+    {
+        id: 2,
+        title: "15 Best Blogs To Follow About Web Design",
+        img: pic1,
+        context: "Tenetur quidem quidem autem quaerat tempora cupiditate. Et consequatur eaque. Quidem et labore adipisci dolorum et eum expedita praesentium. Aliquid atque itaque accusantium maiores. Quasi architecto eum sequi dolores exercitationem incidunt molestiae. Quidem dolorem autem laborum vel voluptatem ut.",
+    },
+    {
+        id: 3,
+        title: "15 Best Blogs To Follow About Web Design",
+        img: pic1,
+        context: "Tenetur quidem quidem autem quaerat tempora cupiditate. Et consequatur eaque. Quidem et labore adipisci dolorum et eum expedita praesentium. Aliquid atque itaque accusantium maiores. Quasi architecto eum sequi dolores exercitationem incidunt molestiae. Quidem dolorem autem laborum vel voluptatem ut.",
+    },
+]
+
+export default blueprintData
