381f48b7d137fd428b70968ec70a09e55c522002
judithdrup <judithdrup@gmail.com>
2020-02-05 22:39:53 -0800

    include url, title, a

--- scrapers/scrapers/spiders/zerowastehome.py
+++ scrapers/scrapers/spiders/zerowastehome.py
@@ -7,15 +7,20 @@ class ZeroWasteHomeSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'https://zerowastehome.com/blog/',
+            'https://zerowastehome.com/blog/page/2/',
+            'https://zerowastehome.com/blog/page/3/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for art in response.css("div.post-content"):
+        for art in response.css("div.post-content"): 
             yield {
-                'link' : art.css("a.entire-meta-link::attr(href)").get(),
-                'title' : art.css("div.post-header a::text").get(),
-                'author' : art.css("div.text a::text").get(),
-                'date' : art.css("div.text span::text").get(),
+                'url' : art.css("a.entire-meta-link::attr(href)").get(),
+                'title' : art.css("div.post-header a::text").get().strip(),
+                'authortags' : art.css("div.text a::text").get(),
+                'image_url' : art.css('div.post-featured-img-wrap span.post-featured-img::attr(style)').re(r'http.*jpg')[0],
+                'content' : art.css("div.excerpt::text").get(),
+                'publish_date' : art.css("div.text span::text").get(),
             }
+                    
